ff1f96e9423a75ef22b47e29b57d0074c01e958e
AdamIx <adamburdik9@gmail.com>
2025-10-29 20:09:53 +0100

    Add: slf4j api dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     id("java")
 }
 
-group = "me.adamix.mercury.configuration"
+group = "me.adamix.mercury.configuration.api"
 version = "0.0.1"
 
 repositories {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-api:2.1.0-alpha1")
 }
